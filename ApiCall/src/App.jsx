a213aa11eb4cdc576ce6bfc1d130bd5fac6937bf
Satyam1722 <72600713+Satyam1722@users.noreply.github.com>
2024-02-14 23:43:42 +0530

    ApiExercise

--- ApiCall/src/App.jsx
+++ ApiCall/src/App.jsx
@@ -4,25 +4,28 @@ import './App.css'
 import { useEffect } from 'react';
 
 function App() {
-  const [data, setdata] = useState(null);
-  const [name, setname] = useState("");
+  const [data, setdata] = useState("");
 
-  
 
-  const fact=(name)=>{
-    Axios.get(`https://api.agify.io/?name=${name}`).then((res)=>{
-      setdata(res.data);
-    })
+  const fact=(type)=>{
+    // Axios.get(`https://excuser-three.vercel.app/v1/excuse/${type}/`).then((res)=>{
+    //   setdata(res.data[0].excuse);
+    // })
+   fetch(`https://excuser-three.vercel.app/v1/excuse/${type}/`).then((res)=>res.json()).then((res)=>{
+    setdata(res[0].excuse);
+   });
+
   }
   
 
   return (
     <>
-      <input type="text" placeholder='Ex.. Satyam' onChange={(e)=>setname(e.target.value)}/>
-      <button onClick={()=>fact(name)}>Click</button>
-      <h1>Name : {data?.name}</h1>
-      <h1>Age : {data?.age}</h1>
-      <h1>Count : {data?.count}</h1>
+      <h1>Generate an Excuse</h1>
+      <button name="family" onClick={(e)=>fact(e.target.name)}>Family</button>
+      <button name="office" onClick={(e)=>fact(e.target.name)}>Office</button>
+      <button name="party" onClick={(e)=>fact(e.target.name)}>Party</button> 
+
+      <h3>{data}</h3>
     </>
   )
 }
